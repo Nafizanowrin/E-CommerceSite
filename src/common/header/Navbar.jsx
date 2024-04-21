127b56fd10318cc333e331a5a9b4b3f405000ff4
Nafizanowrin <115865205+Nafizanowrin@users.noreply.github.com>
2024-04-21 15:11:35 +0600

    Update Navbar.jsx

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -17,22 +17,14 @@ const Navbar = () => {
 
           <div className='navlink'>
             <ul className={MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"} onClick={() => setMobileMenu(false)}>
-              {/*<ul className='link f_flex uppercase {MobileMenu ? "nav-links-MobileMenu" : "nav-links"} onClick={() => setMobileMenu(false)}'>*/}
+             
               <li>
                 <Link to='/'>home</Link>
               </li>
               <li>
                 <Link to='/pages'>pages</Link>
               </li>
-              {/* <li>
-                <Link to='/user'>user account</Link>
-              </li> */}
-              {/* <li>
-                <Link to='/vendor'>vendor account</Link>
-              </li> */}
-              {/* <li>
-                <Link to='/track'>track my order</Link>
-              </li> */}
+ 
               <li>
                 <Link to='/contact'>contact</Link>
               </li>
